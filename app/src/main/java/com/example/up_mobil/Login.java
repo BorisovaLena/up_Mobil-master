114788af69e72ed89c85eabd7c477f3c91e1f78e
BorisovaLena <elenabor2003@gmail.com>
2023-03-09 14:52:22 +0300

    commit

--- app/src/main/java/com/example/up_mobil/Login.java
+++ app/src/main/java/com/example/up_mobil/Login.java
@@ -2,7 +2,9 @@ package com.example.up_mobil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onClickRegister(View v)
+    {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
     }
 }
