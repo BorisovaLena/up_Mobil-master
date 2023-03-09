7e9b7d72e1fb99058b16912fa113a9d2fd59a279
BorisovaLena <elenabor2003@gmail.com>
2023-03-09 16:28:52 +0300

    commit

---
+++ app/src/main/java/com/example/up_mobil/MaskFeelings.java
@@ -0,0 +1,48 @@
+package com.example.up_mobil;
+
+public class MaskFeelings {
+
+    private int id;
+    private String title;
+    private int position;
+    private String image;
+
+    public MaskFeelings(int id, String title, int position, String image) {
+        this.id = id;
+        this.title = title;
+        this.position = position;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
