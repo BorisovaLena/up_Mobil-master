e74af25374c6592d2c26b6c760fef095f511c56f
BorisovaLena <elenabor2003@gmail.com>
2023-03-10 17:34:33 +0300

    profile

--- app/src/main/java/com/example/up_mobil/Profile.java
+++ app/src/main/java/com/example/up_mobil/Profile.java
@@ -2,7 +2,9 @@ package com.example.up_mobil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Profile extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void onClickMain(View v)
+    {
+        Intent intent = new Intent(this, main_page.class);
+        startActivity(intent);
+    }
+
+    public void onClickMenu(View v)
+    {
+        Intent intent = new Intent(this, Menu.class);
+        startActivity(intent);
+    }
+
+    public void onClickLogin(View v)
+    {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
