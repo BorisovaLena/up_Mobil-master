998696c32713dc8fb8017f3719bdc59d6199024d
BorisovaLena <elenabor2003@gmail.com>
2023-03-10 15:30:42 +0300

    переходы

--- app/src/main/java/com/example/up_mobil/main_page.java
+++ app/src/main/java/com/example/up_mobil/main_page.java
@@ -2,7 +2,9 @@ package com.example.up_mobil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class main_page extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class main_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+    }
+
+    public void onClickProfile(View v)
+    {
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
+    }
+
+    public void onClickMenu(View v)
+    {
+        Intent intent = new Intent(this, Menu.class);
+        startActivity(intent);
     }
 }
