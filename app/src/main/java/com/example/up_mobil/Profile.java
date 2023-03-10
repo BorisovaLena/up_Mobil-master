a5fdcb43a9c99117f4f0bf0bb55576431be7c7e4
BorisovaLena <elenabor2003@gmail.com>
2023-03-10 17:40:21 +0300

    10.03.23

--- app/src/main/java/com/example/up_mobil/Profile.java
+++ app/src/main/java/com/example/up_mobil/Profile.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
+
+    ImageView image;
+    TextView Name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        image = findViewById(R.id.avatar);
+        Name = findViewById(R.id.textView13);
+        Name.setText(Login.User.getNickName());
+        new AdapterQoute.DownloadImageTask((ImageView) image).execute(Login.User.getAvatar());
     }
 
     public void onClickMain(View v)
